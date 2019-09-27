7d6c2b50076b2625da43e62f0f98b33a5bd48260
Andres Martin Cantor Urrego <andres.cantor@payulatam.com>
2019-09-27 11:45:19 -0500

    Creation square method

---
+++ src/main/java/edu/escuelaing/arep/services/MathServices.java
@@ -0,0 +1,17 @@
+package edu.escuelaing.arep.services;
+
+/**
+ * @author Andres Martin Cantor Urrego (andres.cantor-u@mail.escuelaing.edu.co)
+ */
+public class MathServices {
+
+    /**
+     * This method return the square of an Integer
+     * @param i the integer that will be operate
+     * @return the square of the integer
+     */
+    public static Integer square(Integer i){
+        return i*i;
+    }
+
+}
